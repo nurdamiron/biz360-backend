fea72b2ee26cc27894503641b08ad099ea64e381
Nurdaulet <nurdamiron@gmail.com>
2025-01-11 23:08:55 +0500

    ывв

--- src/config/database.js
+++ src/config/database.js
@@ -5,10 +5,19 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: 'root',  // Заменили process.env.DB_NAME на 'root'
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
+});
+
+// Добавим логирование для отладки
+pool.on('connection', (connection) => {
+    console.log('New DB connection established');
+});
+
+pool.on('error', (err) => {
+    console.error('Database error:', err);
 });
 
 module.exports = pool;
